36251965ece9659c7a957ec175b3d9058ffa6cbd
Otmane Benlaldj <32651900+otmane24@users.noreply.github.com>
2024-03-11 14:27:50 +0100

    add keystore

--- lib/presentation/colors/color_manager.dart
+++ lib/presentation/colors/color_manager.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 
 class ColorManager {
   static Color primary = HexColor.formHex("#EEC232");
+  static Color secondary = HexColor.formHex("#282828");
+  static Color backgroundColor = HexColor.formHex("#FFFFFF");
+  static Color borderColor = HexColor.formHex("#85808F");
+  static Color textColor = HexColor.formHex("#2B0D66");
+  static Color textFieldTitleColor = HexColor.formHex("#383838");
+  static Color textFieldHintColor = HexColor.formHex("#898989");
+  static Color black = HexColor.formHex("#000000");
+  static Color black2 = HexColor.formHex("#2D2D2D");
+  static Color white = HexColor.formHex("#FFFFFF");
+  static Color amber = HexColor.formHex("#FFB11A");
+  static Color inactiveColor = HexColor.formHex("#D9D9D9");
 }
 
 extension HexColor on Color {
--- lib/routing/app_routing.dart
+++ lib/routing/app_routing.dart
@@ -1,12 +1,4 @@
 class AppRouter {
-  static const String authRouter = '/authRouter';
-  static const String logInRouter = '/logInRouter';
-  static const String forGetPasswordRouter = '/forGetPasswordRouter';
-  static const String emailVerficationRouter = '/emailVerficationRouter';
-  static const String newPaaswordRouter = '/newPaaswordRouter';
-  static const String signUpRouter = '/signUpRouter';
-  static const String optScreenRouter = '/optScreenRouter';
-  static const String completeProfileRouter = '/completeProfileRouter';
-  static const String addAddressRouter = '/addAddressRouter';
-  static const String homeRouter = '/homeRouter';
+
+  static const String homeScreenRouter = '/homeScreenRouter';
 }
